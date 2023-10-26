300ac28a204017cc60203f8bc9aa76b764225f3a
Shefat <shefat2002@gmail.com>
2023-10-27 01:54:22 +0600

    php files added

---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+<div class = "footer">
+        <div class = wrapper>
+            <p class = "text-center">2023 All rights reserved. Developed By - Md Shefat Al Mahmud</p>
+        </div>
+    </div>
+
+
+</body>
+</html>
